f58c7bfa26e400fd8edc73d5bbb1c2712abf0076
Jonathan Sick <jonathansick@mac.com>
2011-09-24 23:49:46 -0400

    added tinygrid.py -- test case of three SSPs

---
+++ tinygrid.py
@@ -0,0 +1,24 @@
+"""Test fspsq with a tiny grid of models--a basic burn in test case."""
+
+from fspsq import FSPSLibrary
+from fspsq import ParameterSet
+
+def main():
+    tinyLibrary = TinySSPGrid("tinyssp", dbname="fsps")
+    tinyLibrary.reset()
+    tinyLibrary.generate_grid()
+    tinyLibrary.compute_models(nThreads=2)
+
+class TinySSPGrid(FSPSLibrary):
+    """A small grid SSPs for three metallicities."""
+    
+    def generate_grid(self):
+        """Create the model grid."""
+        zmets = [1,2,3]
+        for i, zmet in enumerate(zmets):
+            modelName = "model%i" % i
+            pset = ParameterSet(modelName, sfh=0, zmet=zmet)
+            self.register_pset(pset)
+            
+if __name__ == '__main__':
+    main()
