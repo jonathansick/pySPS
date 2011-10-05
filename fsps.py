4ea75c40a77955e99a317573ee745fa236999bd6
Jonathan Sick <jonathansick@mac.com>
2011-10-05 17:08:54 -0400

    FSPSLibrary.create_table() re-write

--- fsps.py
+++ fsps.py
@@ -88,56 +88,69 @@ class FSPSLibrary(object):
         """Create an HDF5 table that combines outputs from models
         in the library.
         """
+        query.update({"compute_complete":True,
+            "np_data": {"$exists": 1}})
+        docs = self.collection.find(query) # , limit=2
+        print "working on %i docs to read" % docs.count()
+        lut = get_metallicity_LUT(isocType, specType)
+        # TODO need to generalize definition of columns. A user ought to
+        # be able to use any pset columns, any set of mags, and the spectra
+        #magNames = ['TMASS_J','TMASS_H','TMASS_Ks','MegaCam_u','MegaCam_g',
+        #        'MegaCam_r','MegaCam_i','MegaCam_z','GALEX_NUV','GALEX_FUV']
+        magCols = [(s,np.float) for (i,s,c) in FILTER_LIST]
+        #magCols = [(s,np.float) for s in magNames]
+        psetCols = [('dust_type',np.int),('imf_type',np.int),('sfh',np.int),
+                ('tau',np.float),('const',np.float),('sf_start',np.float),
+                ('fburst',np.float),('tburst',np.float),('dust_tesc',np.float),
+                ('dust1',np.float),('dust2',np.float),('frac_nodust',np.float)]
+        sfhCols = [('age',np.float),('mass',np.float),('lbol',np.float),
+                ('sfr',np.float)]
+        miscCols = [('Z',np.float)] # metallicity, taken from zmet look-up-table
+        specCols = [('spec',np.float,SpecParser.nlambda(specType))]
+        tableDtype = np.dtype(psetCols+sfhCols+miscCols+magCols+specCols)
+
         if os.path.exists(outputPath) and clobber:
             os.remove(outputPath)
         title = os.path.splitext(os.path.basename(outputPath))[0]
         h5file = tables.openFile(outputPath, mode="w", title=title)
-        table = h5file.createTable("/", 'mags', self._mag_table_def,
+        table = h5file.createTable("/", 'models', tableDtype,
                 "Model Output Table")
         print h5file
-        docs = self.collection.find({"compute_complete":True,
-            "np_data": {"$exists": 1}}) # , limit=2
-        print "working on %i docs to read" % docs.count()
-        lut = get_metallicity_LUT(isocType, specType)
-        rows = []
-        cols = ['Z','tau','age','mass','lbol','sfr','TMASS_J','TMASS_H',
-                'TMASS_Ks','MegaCam_u','MegaCam_g','MegaCam_r','MegaCam_i',
-                'MegaCam_z','GALEX_NUV','GALEX_FUV']
         for doc in docs:
             print "reading", doc['_id']
-            # print doc.keys()
-            # print doc['np_data']
             npData = doc['np_data']
-            # print npData.dtype
-            # binData = Binary(doc['np_data']['data'])
-            # print type(binData)
-            # npData = pickle.load(binData)
             nRows = len(npData)
-            # Append model information (about SFH, dust, etc)
+            
+            # Appent pset cols and misc cols
+            extraNames = []
+            extraArrays = []
+
             zmet = doc['pset']['zmet']
             Z = lut[zmet-1]
-            zmets = np.ones(nRows, dtype=np.float) * Z
-            tau = doc['pset']['tau']
-            taus = np.ones(nRows, dtype=np.float) * tau
-            npDataAll = mlab.rec_append_fields(npData, ['Z','tau'],[zmets,taus])
+            Z = np.ones(nRows, dtype=np.float) * Z
+            extraNames.append('Z')
+            extraArrays.append(Z)
+            for cName, cType in psetCols:
+                p = doc['pset'][cName]
+                pArray = np.ones(nRows, dtype=cType) * p
+                extraNames.append(cName)
+                extraArrays.append(pArray)
+            npDataAll = mlab.rec_append_fields(npData, extraNames, extraArrays)
+            print "npDataAll dtype:", npDataAll.dtype
+
             # Trim the recarray to just the desired fields
-            npDataTrim = mlab.rec_keep_fields(npDataAll,
-                ['Z','tau','age','mass','lbol','sfr','TMASS_J','TMASS_H',
-                'TMASS_Ks','MegaCam_u','MegaCam_g','MegaCam_r','MegaCam_i',
-                'MegaCam_z','GALEX_NUV','GALEX_FUV'])
+            #npDataTrim = mlab.rec_keep_fields(npDataAll,
+            #    ['Z','tau','age','mass','lbol','sfr','TMASS_J','TMASS_H',
+            #    'TMASS_Ks','MegaCam_u','MegaCam_g','MegaCam_r','MegaCam_i',
+            #    'MegaCam_z','GALEX_NUV','GALEX_FUV'])
             # select row closest to the target age
-            ageGyr = 10.**npDataTrim['age'] / 10.**9
-            i = np.argmin((ageGyr - t)**2)
-            row = np.array(npDataTrim[i], copy=True)
-            print i, row.shape, row.dtype
-            print row['Z'], row['tau'],row['TMASS_J'],row['TMASS_Ks']
-            rows.append(row)
-            # Append to HDF5
-            for col in cols:
-                table.row[col] = row[col]
-            table.row.append()
-            #table.append(npDataTrim)
-        #mlab.recs_join(key, name, rows, jointype='outer', missing=0.0, postfixes=None)
+            if tage is not None:
+                ageGyr = 10.**npDataAll['age'] / 10.**9
+                i = np.argmin((ageGyr - tage)**2)
+                row = np.atleast_1d(np.array(npDataAll[i], copy=True))
+                table.append(row)
+            else:
+                table.append(npDataAll)
         h5file.flush()
         h5file.close()
 
@@ -271,7 +284,7 @@ class QueueRunner(object):
             magData = magParser.data
             specParser = SpecParser(specPath)
             specData = specParser.data
-            nLambda = specParser._get_nlambda()
+            nLambda = specParser.nlambda('basel')
             if getMags and getSpec:
                 allData = self._splice_mag_spec_arrays(magData, specData, nLambda)
             #except:
@@ -340,7 +353,7 @@ class SpecParser(object):
         #super(SpecParser, self).__init__()
         self.specPath = specPath
         self.specType = specType
-        nLambda = self._get_nlambda()
+        nLambda = SpecParser.nlambda(specType)
         
         f = open(specPath, 'r')
         allLines = f.readlines()
@@ -358,15 +371,16 @@ class SpecParser(object):
             spec = [float(x) for x in allLines[iSpec].split()]
             self.data['spec'][i] = spec
         f.close()
-
-    def _get_nlambda(self):
+    
+    @classmethod
+    def nlambda(cls, specType):
         """Get the number of wavelengths expected in this spectrum."""
-        if self.specType == 'basel':
+        if specType == 'basel':
             return 1221
-        elif self.specType == 'miles':
+        elif specType == 'miles':
             return 4222
         else:
-            assert "spec type invalid:", self.specType
+            assert "spec type invalid:", specType
 
     @property
     def wavelengths(self):
