fc37a3833b750f78ea89d3b0658143b1373b8e1b
Jonathan Sick <jonathansick@mac.com>
2011-12-26 13:44:39 -0700

    fsps.f90 comp_sp() routine and test_fsps.py burn-in script.

--- fsps.f90
+++ fsps.f90
@@ -9,26 +9,16 @@ module fsps
     type(COMPSPOUT), dimension(ntfull) :: ocompsp
     integer :: zi, my_foo_var
 
-    !integer, intent(in) :: _imf_type, _dust_type, _zmet, _sfh,
-    
-    !real, intent(in) :: _imf1, imf2, _imf3, _vdmc, _mdave,
-    !real, intent(in) ::_dell, _delt, _fbhb, _pagb
-    !real, intent(in) :: _tau, _const, _tage, _fburst, _tburst,
-    !real, intent(in) :: _dust_tesc, _dust1, _dust2
-    !real :: _dust_clumps, _frac_no_dust, _dust_index, _mwr
-    !real :: _wgp1, _wgp2, _wgp3, _duste_gamma, _duste_qpah
-    !character(len=256), intent(in) :: model_name,
-    !character(len=256) :: output_path
-
 contains
     ! General setup that loads isochrones and stellar libraries
     subroutine setup(use_vega_mags, apply_redshift)
         integer, intent(in) :: use_vega_mags, apply_redshift
+        write (*,*) 'starting setup()'
         compute_vega_mags = use_vega_mags
         redshift_colors = apply_redshift
         ! Load isochrones and stellar libraries
         call sps_setup(-1)
-        write (*,*) 'Hello, exquisitely brave world!'
+        write (*,*) 'setup() complete'
     end subroutine
 
     subroutine setup_all_ssp(imf, imf1, imf2, imf3, vdmc, &
@@ -57,38 +47,49 @@ contains
             call ssp_gen(pset,mass_ssp_zz(zi,:), &
                 lbol_ssp_zz(zi,:), spec_ssp_zz(zi,:,:))
         end do
+        write (*,*) 'setup_all_ssp() complete'
     end subroutine
 
-    !subroutine comp_sp(model_name, _dust_type, _zmet, _sfh, _tau, _const, &
-    !        _fburst, _tburst, _dust_tesc, _dust1, _dust2, _dust_clumps, &
-    !        _frac_no_dust, _dust_index, _mwr, _wgp1, _wgp2, _wgp3, &
-    !        _duste_gamma, _duste_umin, _duste_qpah, _tage)
-
-    !    dust_type = _dust_type
-    !    pset%zmet = _zmet
-    !    pset%sfh = _sfh
-    !    pset%tau = _tau
-    !    pset%const = _const
-    !    pset%tage = _tage
-    !    pset%fburst = _fburst
-    !    pset%tburst = _tburst
-    !    pset%dust_tesc = _dust_tesc
-    !    pset%dust1 = _dust1
-    !    pset%dust2 = _dust2
-    !    pset%dust_clumps = _dust_clumps
-    !    pset%frac_no_dust = _frac_no_dust
-    !    pset%dust_index = _dust_index
-    !    pset%mwr = _mwr
-    !    pset%wgp1 = _wgp1
-    !    pset%wgp2 = _wgp2
-    !    pset%wpg3 = _wgp3
-    !    pset%duste_gamma = _duste_gamma
-    !    pset%duste_umin = _duste_qpah
-    !    ! For calling COMPSP
-    !    ! depends on zmet, SFH, dust
-    !    ! we want to directly return the results, for now use file output
-    !    output_path = trim(model_name)//'.out'
-    !    call compsp(3,1,output_path,mass_ssp,lbol_ssp,spec_ssp,pset,ocompsp)
-    !end subroutine
+    subroutine comp_sp(model_name, dust, zmet, sfh, tau, const, &
+            fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, &
+            frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, &
+            duste_gamma, duste_umin, duste_qpah, tage)
+        character(len=256) :: output_path
+        character(len=256), intent(in) :: model_name
+        integer, intent(in) :: dust, zmet, sfh
+        real, intent(in) :: tau, const, fburst, tburst, dust_tesc
+        real, intent(in) :: dust1, dust2, dust_clumps, frac_no_dust
+        real, intent(in) :: dust_index, mwr
+        integer, intent(in) :: wgp1, wgp2, wgp3
+        real, intent(in) :: duste_gamma, duste_umin, duste_qpah, tage
+        dust_type = dust
+        pset%zmet = zmet
+        pset%sfh = sfh
+        pset%tau = tau
+        pset%const = const
+        pset%tage = tage
+        pset%fburst = fburst
+        pset%tburst = tburst
+        pset%dust_tesc = dust_tesc
+        pset%dust1 = dust1
+        pset%dust2 = dust2
+        pset%dust_clumps = dust_clumps
+        pset%frac_nodust = frac_no_dust
+        pset%dust_index = dust_index
+        pset%mwr = mwr
+        pset%wgp1 = wgp1
+        pset%wgp2 = wgp2
+        pset%wgp3 = wgp3
+        pset%duste_gamma = duste_gamma
+        pset%duste_umin = duste_umin
+        pset%duste_qpah = duste_qpah
+        ! For calling COMPSP
+        ! depends on zmet, SFH, dust
+        ! we want to directly return the results, for now use file output
+        output_path = trim(model_name)//'.out'
+        call compsp(3, 1, output_path, mass_ssp_zz(zmet,:), &
+            lbol_ssp_zz(zmet,:), spec_ssp_zz(zmet,:,:), pset, ocompsp)
+        write (*,*) 'comp_sp() complete'
+    end subroutine
 
 end module
---
+++ test_fsps.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""Burn-in tests for the fsps f2py wrapper"""
+
+import fsps
+
+def main():
+    use_vega_mags, apply_redshift = 0, 0
+
+    imf, imf1, imf2, imf3, vdmc, mdave, dell, delt, sbss, fbhb, pagb = \
+        0,1.3,2.3,2.3,0.08,0.5,0.,0.,0.,0.,1.
+            
+    model_name = "test_fsps"
+    dust_type = 0
+    zmet = 0
+    sfh = 1
+    tau = 0.5
+    const = 0.1
+    fburst = 0.
+    tburst = 0.
+    dust_tesc = 7.
+    dust1 = 0.
+    dust2 = 0.
+    dust_clumps = -99.
+    frac_no_dust = 0.1
+    dust_index = -0.7,
+    mwr = 3.1
+    wgp1 = 1
+    wgp2 = 1
+    wgp3 = 1
+    duste_gamma = 0.01
+    duste_umin = 1.0
+    duste_qpah = 3.5
+    tage = 0.
+            
+    fsps.fsps.setup(use_vega_mags, apply_redshift)
+    fsps.fsps.setup_all_ssp(imf, imf1, imf2, imf3, vdmc, mdave, dell, delt,
+            sbss, fbhb, pagb)
+    fsps.fsps.comp_sp(model_name, dust_type, zmet, sfh, tau, const, 
+            fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, 
+            frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, 
+            duste_gamma, duste_umin, duste_qpah, tage)
+    print "All tests complete!"
+
+if __name__ == '__main__':
+    main()
+
+
