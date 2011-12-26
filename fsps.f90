0ed0b43122f35e4abb5d800c8752742f8518e618
Jonathan Sick <jonathansick@mac.com>
2011-12-26 15:55:20 -0700

    fsps.f90 return scalar args for number of filters, masses, ages

--- fsps.f90
+++ fsps.f90
@@ -50,7 +50,8 @@ contains
         write (*,*) 'setup_all_ssp() complete'
     end subroutine
 
-    subroutine comp_sp(model_name, dust, zmet, sfh, tau, const, &
+    ! TODO directly return outputs from the COMPSPOUT structure
+    subroutine comp_sp_file(model_name, dust, zmet, sfh, tau, const, &
             fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, &
             frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, &
             duste_gamma, duste_umin, duste_qpah, tage)
@@ -89,7 +90,67 @@ contains
         output_path = trim(model_name)//'.out'
         call compsp(3, 1, output_path, mass_ssp_zz(zmet,:), &
             lbol_ssp_zz(zmet,:), spec_ssp_zz(zmet,:,:), pset, ocompsp)
+        write (*,*) 'comp_sp_file() complete'
+    end subroutine
+
+    ! Computes the SP, keeping the results to memory
+    ! Call get_sp_mags() or get_sp_spec() to get outputs
+    subroutine comp_sp(dust, zmet, sfh, tau, const, &
+            fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, &
+            frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, &
+            duste_gamma, duste_umin, duste_qpah, tage)
+        integer, intent(in) :: dust, zmet, sfh
+        real, intent(in) :: tau, const, fburst, tburst, dust_tesc
+        real, intent(in) :: dust1, dust2, dust_clumps, frac_no_dust
+        real, intent(in) :: dust_index, mwr
+        integer, intent(in) :: wgp1, wgp2, wgp3
+        real, intent(in) :: duste_gamma, duste_umin, duste_qpah, tage
+        dust_type = dust
+        pset%zmet = zmet
+        pset%sfh = sfh
+        pset%tau = tau
+        pset%const = const
+        pset%tage = tage
+        pset%fburst = fburst
+        pset%tburst = tburst
+        pset%dust_tesc = dust_tesc
+        pset%dust1 = dust1
+        pset%dust2 = dust2
+        pset%dust_clumps = dust_clumps
+        pset%frac_nodust = frac_no_dust
+        pset%dust_index = dust_index
+        pset%mwr = mwr
+        pset%wgp1 = wgp1
+        pset%wgp2 = wgp2
+        pset%wgp3 = wgp3
+        pset%duste_gamma = duste_gamma
+        pset%duste_umin = duste_umin
+        pset%duste_qpah = duste_qpah
+        ! For calling COMPSP
+        ! depends on zmet, SFH, dust
+        ! we want to directly return the results, for now use file output
+        !output_path = trim(model_name)//'.out'
+        !call compsp(3, 1, output_path, mass_ssp_zz(zmet,:), &
+        !    lbol_ssp_zz(zmet,:), spec_ssp_zz(zmet,:,:), pset, ocompsp)
         write (*,*) 'comp_sp() complete'
+    end subroutine
+
+    ! Returns number of filters that mags are computed for
+    subroutine get_n_bands(n_bands)
+        integer, intent(out) :: n_bands
+        n_bands = nbands
+    end subroutine
+
+    ! Returns number of ages that are computed in a SFH, if tage is set to 0.
+    subroutine get_n_ages(n_ages)
+        integer, intent(out) :: n_ages
+        n_ages = ntfull
+    end subroutine
+
+    ! Returns number of masses included in the isochrones
+    subroutine get_n_masses(n_masses)
+        integer, intent(out) :: n_masses
+        n_masses = nm
     end subroutine
 
 end module
--- test_fsps.py
+++ test_fsps.py
@@ -34,12 +34,18 @@ def main():
     tage = 0.
             
     fsps.fsps.setup(use_vega_mags, apply_redshift)
+    nBands = fsps.fsps.get_n_bands()
+    nAges = fsps.fsps.get_n_ages()
+    nMasses = fsps.fsps.get_n_masses()
     fsps.fsps.setup_all_ssp(imf, imf1, imf2, imf3, vdmc, mdave, dell, delt,
             sbss, fbhb, pagb)
-    fsps.fsps.comp_sp(model_name, dust_type, zmet, sfh, tau, const, 
+    fsps.fsps.comp_sp(dust_type, zmet, sfh, tau, const, 
             fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, 
             frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, 
             duste_gamma, duste_umin, duste_qpah, tage)
+    print "There are", nBands, "bands"
+    print "There are", nAges, "ages"
+    print "There are", nMasses, "masses"
     print "All tests complete!"
 
 if __name__ == '__main__':
