867545e2da96a3687681f523bb06d2f3b662ac66
Jonathan Sick <jonathansick@mac.com>
2011-12-29 17:44:19 -0700

    fsps.f90: deleted comp_sp_file, renamed cap subroutines with cap tag

--- fsps.f90
+++ fsps.f90
@@ -9,7 +9,7 @@ module fsps
     !f2py intent(hide) pset
     type(COMPSPOUT), dimension(ntfull) :: ocompsp
     !f2py intent(hide) ocompsp
-    integer :: zi, my_foo_var
+    integer :: zi
 
 contains
     ! General setup that loads isochrones and stellar libraries
@@ -50,49 +50,6 @@ contains
                 lbol_ssp_zz(zi,:), spec_ssp_zz(zi,:,:))
         end do
         write (*,*) 'setup_all_ssp() complete'
-    end subroutine
-
-    ! TODO directly return outputs from the COMPSPOUT structure
-    subroutine comp_sp_file(model_name, dust, zmet, sfh, tau, const, &
-            fburst, tburst, dust_tesc, dust1, dust2, dust_clumps, &
-            frac_no_dust, dust_index, mwr, wgp1, wgp2, wgp3, &
-            duste_gamma, duste_umin, duste_qpah, tage)
-        character(len=256) :: output_path
-        character(len=256), intent(in) :: model_name
-        integer, intent(in) :: dust, zmet, sfh
-        real, intent(in) :: tau, const, fburst, tburst, dust_tesc
-        real, intent(in) :: dust1, dust2, dust_clumps, frac_no_dust
-        real, intent(in) :: dust_index, mwr
-        integer, intent(in) :: wgp1, wgp2, wgp3
-        real, intent(in) :: duste_gamma, duste_umin, duste_qpah, tage
-        dust_type = dust
-        pset%zmet = zmet
-        pset%sfh = sfh
-        pset%tau = tau
-        pset%const = const
-        pset%tage = tage
-        pset%fburst = fburst
-        pset%tburst = tburst
-        pset%dust_tesc = dust_tesc
-        pset%dust1 = dust1
-        pset%dust2 = dust2
-        pset%dust_clumps = dust_clumps
-        pset%frac_nodust = frac_no_dust
-        pset%dust_index = dust_index
-        pset%mwr = mwr
-        pset%wgp1 = wgp1
-        pset%wgp2 = wgp2
-        pset%wgp3 = wgp3
-        pset%duste_gamma = duste_gamma
-        pset%duste_umin = duste_umin
-        pset%duste_qpah = duste_qpah
-        ! For calling COMPSP
-        ! depends on zmet, SFH, dust
-        ! we want to directly return the results, for now use file output
-        output_path = trim(model_name)//'.out'
-        call compsp(3, 1, output_path, mass_ssp_zz(zmet,:), &
-            lbol_ssp_zz(zmet,:), spec_ssp_zz(zmet,:,:), pset, ocompsp)
-        write (*,*) 'comp_sp_file() complete'
     end subroutine
 
     ! Computes the SP, keeping the results to memory
@@ -175,7 +132,7 @@ contains
     ! todo figure out how this interacts with setting tage; does ocompsp
     ! simply place all data at the first index? Or at the proper index?
     ! Or is tage only related to writing output files?
-    subroutine get_mags_at_age(iage, n_bands, mag_array)
+    subroutine get_csp_mags_at_age(iage, n_bands, mag_array)
         integer, intent(in) :: iage, n_bands
         real, dimension(n_bands), intent(out) :: mag_array
         write (*,*) 'in get_mags_at_age'
@@ -188,7 +145,7 @@ contains
 
     ! Get the age, mass, bolometric luminosity, SFR and dust mass at
     ! a single age
-    subroutine get_stats_at_age(iage, age, mass, lbol, sfr, dust_mass)
+    subroutine get_csp_stats_at_age(iage, age, mass, lbol, sfr, dust_mass)
         integer, intent(in) :: iage
         real, intent(out) :: age, mass, lbol, sfr, dust_mass
         age = ocompsp(iage)%age
@@ -199,7 +156,7 @@ contains
     end subroutine
 
     ! Get mags for all ages
-    subroutine get_mags(n_bands, n_ages, mag_array)
+    subroutine get_csp_mags(n_bands, n_ages, mag_array)
         integer, intent(in) :: n_bands, n_ages
         real, dimension(n_bands,n_ages), intent(out) :: mag_array
         do zi=1,n_ages
@@ -208,7 +165,7 @@ contains
     end subroutine
 
     ! Get stellar pop statistics for all ages
-    subroutine get_stats(n_ages, age, mass, lbol, sfr, dust_mass)
+    subroutine get_csp_stats(n_ages, age, mass, lbol, sfr, dust_mass)
         integer, intent(in) :: n_ages
         real, dimension(n_ages), intent(out) :: age, mass, lbol, sfr, dust_mass
         do zi=1,n_ages
