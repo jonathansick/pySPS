cee0ce9d32d567090113cbd759ea048a27f72ec9
Jonathan Sick <jonathansick@mac.com>
2012-04-25 18:02:18 -0400

    Added build_fsps setup.py build command

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from setuptools import setup
+from setuptools import setup, Command
 from sphinx.setup_command import BuildDoc
+
+import os
+import shutil
+import glob
+import subprocess
+
 
 dependencies = """
 sphinx
@@ -9,7 +15,61 @@ numpy
 pymongo
 """
 
-cmdclass = {'build_sphinx': BuildDoc}
+class BuildFSPS(Command):
+    """setuptools command for compiling the fsps extension."""
+    description = "Build FSPS and the fsps extension module"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """docstring for run"""
+        fspsDir = os.environ["SPS_HOME"]
+
+        # Make FSPS
+        subprocess.call("cd %s;make"%os.path.join(fspsDir,"src"), shell=True)
+
+        # Gather all binaries built with FSPS
+        fspsSrcs = glob.glob(os.path.join(fspsDir,"src/*.o"))
+        oPaths = fspsSrcs
+        modPaths = glob.glob(os.path.join(fspsDir,"src/*.mod"))
+        filterNames = [os.path.join(fspsDir, 'src', n) for n in
+                ['simple.o','lesssimple.o','autosps.o']]
+        for n in filterNames:
+            oPaths = [p for p in oPaths if n not in p]
+
+        srcDir = "pysps/_fsps_src"
+        if not os.path.exists(srcDir): os.makedirs(srcDir)
+        copiedOPaths = []
+        newModPaths = []
+        for p in oPaths:
+            basename = os.path.basename(p)
+            shutil.copy(p, os.path.join(srcDir, basename))
+        for p in modPaths:
+            basename = os.path.basename(p)
+            newPath = os.path.join("pysps", basename)
+            newModPaths.append(newPath)
+            shutil.copy(p, newPath)
+            oPaths = glob.glob(os.path.join(srcDir, "%.o"))
+
+        subprocess.call("cd pysps;f2py fsps.f90 -m fsps -h fsps.pyf --overwrite-signature",
+                shell=True)
+
+        # apparently gnu95 implies gfortran
+        cmd = "cd pysps;f2py -c --fcompiler=gnu95 fsps.pyf fsps.f90 %s" % " ".join(oPaths)
+        subprocess.call(cmd, shell=True)
+
+        # Cleanup
+        for p in newModPaths:
+            os.remove(p)
+        shutil.rmtree(srcDir)
+
+
+cmdclass = {'build_sphinx': BuildDoc, 'build_fsps': BuildFSPS}
 
 setup(
     name='pysps',
