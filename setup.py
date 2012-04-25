fcfa99a25a227fd283aa2b03fb072777d418be8a
Jonathan Sick <jonathansick@mac.com>
2012-04-25 17:17:19 -0400

    fleshed out setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from setuptools import setup
-setup(name='pysps')
+
+dependencies = """
+sphinx
+numpy
+pymongo
+"""
+
+setup(
+    name='pysps',
+    version=0.2,
+    author='Jonathan Sick',
+    author_email='jonathansick@mac.com',
+    description='Interface to FSPS, the Flexible Stellar Population Synthesis package',
+    license='BSD',
+    install_requires=dependencies.split()
+)
